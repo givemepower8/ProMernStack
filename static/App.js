7559bf05dd21db902e781712279289a80e9671bc
Wei Wang <wei.wang@ia.ca>
2019-02-26 10:40:29 -0800

    chapter 4

--- static/App.js
+++ static/App.js
@@ -10,115 +10,137 @@ class IssueFilter extends React.Component {
   }
 }
 
-class IssueRow extends React.Component {
-  render() {
-    const { issue } = this.props;
-    return React.createElement(
-      'tr',
+const IssueRow = props => React.createElement(
+  'tr',
+  null,
+  React.createElement(
+    'td',
+    null,
+    props.issue.id
+  ),
+  React.createElement(
+    'td',
+    null,
+    props.issue.status
+  ),
+  React.createElement(
+    'td',
+    null,
+    props.issue.owner
+  ),
+  React.createElement(
+    'td',
+    null,
+    props.issue.created.toDateString()
+  ),
+  React.createElement(
+    'td',
+    null,
+    props.issue.effort
+  ),
+  React.createElement(
+    'td',
+    null,
+    props.issue.completionDate ? props.issue.completionDate.toDateString() : ''
+  ),
+  React.createElement(
+    'td',
+    null,
+    props.issue.title
+  )
+);
+
+function IssueTable(props) {
+  const issueRows = props.issues.map(issue => React.createElement(IssueRow, { key: issue.id, issue: issue }));
+  return React.createElement(
+    'table',
+    { className: 'bordered-table' },
+    React.createElement(
+      'thead',
       null,
       React.createElement(
-        'td',
-        null,
-        issue.id
-      ),
-      React.createElement(
-        'td',
-        null,
-        issue.status
-      ),
-      React.createElement(
-        'td',
-        null,
-        issue.owner
-      ),
-      React.createElement(
-        'td',
-        null,
-        issue.created.toDateString()
-      ),
-      React.createElement(
-        'td',
-        null,
-        issue.effort
-      ),
-      React.createElement(
-        'td',
-        null,
-        issue.completionDate ? issue.completionDate.toDateString() : ''
-      ),
-      React.createElement(
-        'td',
-        null,
-        issue.title
-      )
-    );
-  }
-}
-
-class IssueTable extends React.Component {
-  render() {
-    const issueRows = this.props.issues.map(issue => React.createElement(IssueRow, { key: issue.id, issue: issue }));
-    return React.createElement(
-      'table',
-      { className: 'bordered-table' },
-      React.createElement(
-        'thead',
+        'tr',
         null,
         React.createElement(
-          'tr',
+          'th',
           null,
-          React.createElement(
-            'th',
-            null,
-            'Id'
-          ),
-          React.createElement(
-            'th',
-            null,
-            'Status'
-          ),
-          React.createElement(
-            'th',
-            null,
-            'Owner'
-          ),
-          React.createElement(
-            'th',
-            null,
-            'Created'
-          ),
-          React.createElement(
-            'th',
-            null,
-            'Effort'
-          ),
-          React.createElement(
-            'th',
-            null,
-            'Completion Date'
-          ),
-          React.createElement(
-            'th',
-            null,
-            'Title'
-          )
+          'Id'
+        ),
+        React.createElement(
+          'th',
+          null,
+          'Status'
+        ),
+        React.createElement(
+          'th',
+          null,
+          'Owner'
+        ),
+        React.createElement(
+          'th',
+          null,
+          'Created'
+        ),
+        React.createElement(
+          'th',
+          null,
+          'Effort'
+        ),
+        React.createElement(
+          'th',
+          null,
+          'Completion Date'
+        ),
+        React.createElement(
+          'th',
+          null,
+          'Title'
         )
-      ),
-      React.createElement(
-        'tbody',
-        null,
-        issueRows
       )
-    );
-  }
+    ),
+    React.createElement(
+      'tbody',
+      null,
+      issueRows
+    )
+  );
 }
 
 class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
+    this.props.createIssue({
+      owner: form.owner.value,
+      title: form.title.value,
+      status: 'New',
+      created: new Date()
+    });
+    // clear the form for the next input
+    form.owner.value = '';
+    form.title.value = '';
+  }
+
   render() {
     return React.createElement(
       'div',
       null,
-      'This is a placeholder for an Issue Add entry form.'
+      React.createElement(
+        'form',
+        { name: 'issueAdd', onSubmit: this.handleSubmit },
+        React.createElement('input', { type: 'text', name: 'owner', placeholder: 'Owner' }),
+        React.createElement('input', { type: 'text', name: 'title', placeholder: 'Title' }),
+        React.createElement(
+          'button',
+          null,
+          'Add'
+        )
+      )
     );
   }
 }
@@ -142,6 +164,30 @@ const issues = [{
 }];
 
 class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({ issues });
+    }, 500);
+  }
+
+  createIssue(newIssue) {
+    const newIssues = this.state.issues.slice();
+    newIssue.id = this.state.issues.length + 1;
+    newIssues.push(newIssue);
+    this.setState({ issues: newIssues });
+  }
+
   render() {
     return React.createElement(
       'div',
@@ -153,9 +199,9 @@ class IssueList extends React.Component {
       ),
       React.createElement(IssueFilter, null),
       React.createElement('hr', null),
-      React.createElement(IssueTable, { issues: issues }),
+      React.createElement(IssueTable, { issues: this.state.issues }),
       React.createElement('hr', null),
-      React.createElement(IssueAdd, null)
+      React.createElement(IssueAdd, { createIssue: this.createIssue })
     );
   }
 }
